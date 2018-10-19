59147066bfd22d7788cfc5fafac2bc635253c77b
刘茹 <liuru621@163.com>
2018-10-19 17:43:59 +0800

    tijiao

--- works/website/js/four.js
+++
@@ -1,5 +0,0 @@
-$(".main .menu .menu-name li").click(function () {
-    var index=$(this).index();
-    $(this).addClass("active").siblings().removeClass("active");
-    $(".mainbox>ul>li").css("display","none").eq(index).css("display","block");
-})
